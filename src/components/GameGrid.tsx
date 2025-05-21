cf8480a226a77e755b3909cf06c37e2bfc78f81f
Muzammil <muzammilghani1994@gmail.com>
2025-05-21 20:16:45 +0500

    More Changes

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { Text } from "@chakra-ui/react";
 import apiClient from "../services/api-client";
-
 interface Game {
   id: number;
   name: string;
@@ -10,14 +9,13 @@ interface FetchGamesResponse {
   count: number;
   results: Game[];
 }
-
 const GameGrid = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
 
   useEffect(() => {
     apiClient
-      .get<FetchGamesResponse>("/games")
+      .get("/games")
       .then((res) => setGames(res.data.results))
       .catch((error) => {
         setError(error.message);
