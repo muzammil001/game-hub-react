ca57913e871adc08a7595e04afeba3868de1d406
Muzammil <muzammilghani1994@gmail.com>
2025-05-23 07:08:24 +0500

    Show Loading Skeletons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -8,18 +8,24 @@ import gameService, {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
   useEffect(() => {
+    setLoading(true);
     const { request, cancel } = gameService.get<FetchGamesResponse>();
     request
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
       .catch((error) => {
         if (error instanceof CanceledError) return;
         setError(error.message);
+        setLoading(false);
       });
     return () => cancel();
   }, []);
 
-  return { games, error };
+  return { games, error, isloading: isLoading };
 };
 
 export default useGames;
