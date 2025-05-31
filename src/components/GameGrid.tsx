3ffa5f6ff10730b34289ca1c7ba0c2dcb9882325
Muzammil <muzammilghani1994@gmail.com>
2025-05-31 09:20:00 +0500

    Add Load more in games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,23 +1,36 @@
-import { SimpleGrid, Text } from "@chakra-ui/react";
+import { Button, SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import type { GameQuery } from "../App";
+import React from "react";
 interface Props {
   gameQuery: GameQuery | null;
 }
 const GameGrid = ({ gameQuery }: Props) => {
-  const { data, error, isLoading } = useGames(gameQuery);
+  const { data, error, isLoading, isFetchingNextPage, fetchNextPage } =
+    useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
   return (
     <>
       {error && <Text>{error.message}</Text>}
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={3}>
         {isLoading && skeletons.map((s) => <GameCardSkeleton key={s} />)}
-        {data?.results.map((game) => (
-          <GameCard key={game.id} game={game}></GameCard>
+        {data?.pages.map((page, index) => (
+          <React.Fragment key={index}>
+            {page.results?.map((game) => (
+              <GameCard key={game.id} game={game}></GameCard>
+            ))}
+          </React.Fragment>
         ))}
       </SimpleGrid>
+      <Button
+        disabled={isFetchingNextPage}
+        className="btn btn-primary ms-2 my-3"
+        onClick={() => fetchNextPage()}
+      >
+        {isFetchingNextPage ? "Loading..." : "Load More"}
+      </Button>
     </>
   );
 };
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,21 +1,26 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import type { GameQuery } from "../App";
 import { type FetchResponse } from "../services/api-client";
 import type { Game } from "../services/game-service";
 import gameService from "../services/game-service";
 
 const useGames = (gameQuery: GameQuery | null) => {
-  return useQuery<FetchResponse<Game>, Error>({
+  return useInfiniteQuery<FetchResponse<Game>, Error>({
     queryKey: ["games", gameQuery],
-    queryFn: () =>
+    queryFn: ({ pageParam = 1 }) =>
       gameService.get({
         params: {
           genres: gameQuery?.genre?.id,
           parent_platforms: gameQuery?.platform?.id,
           ordering: gameQuery?.sortOrder,
           search: gameQuery?.searchText,
+          page: pageParam,
         },
       }),
+    getNextPageParam: (lastPage, allPages) => {
+      return lastPage.results.length > 0 ? allPages.length + 1 : undefined;
+    },
+    initialPageParam: 1, //THIS LINE HERE!
     staleTime: 24 * 60 * 60 * 1000, //24h
   });
 };
