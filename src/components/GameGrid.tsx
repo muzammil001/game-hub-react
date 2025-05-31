cf5b11a4f8dad5a08f8df841d5eb31029e11e21a
Muzammil <muzammilghani1994@gmail.com>
2025-05-31 09:49:15 +0500

    implement infinite scroll

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,36 +1,43 @@
-import { Button, SimpleGrid, Text } from "@chakra-ui/react";
+import { SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import type { GameQuery } from "../App";
+import InfiniteScroll from "react-infinite-scroll-component";
+
 import React from "react";
 interface Props {
   gameQuery: GameQuery | null;
 }
 const GameGrid = ({ gameQuery }: Props) => {
-  const { data, error, isLoading, isFetchingNextPage, fetchNextPage } =
+  const { data, error, isLoading, fetchNextPage, hasNextPage } =
     useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
+  const fetchedGamesCount = data?.pages?.reduce(
+    (acc, page) => acc + page?.results?.length,
+    0
+  );
   return (
     <>
       {error && <Text>{error.message}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={3}>
-        {isLoading && skeletons.map((s) => <GameCardSkeleton key={s} />)}
-        {data?.pages.map((page, index) => (
-          <React.Fragment key={index}>
-            {page.results?.map((game) => (
-              <GameCard key={game.id} game={game}></GameCard>
-            ))}
-          </React.Fragment>
-        ))}
-      </SimpleGrid>
-      <Button
-        disabled={isFetchingNextPage}
-        className="btn btn-primary ms-2 my-3"
-        onClick={() => fetchNextPage()}
+      <InfiniteScroll
+        dataLength={fetchedGamesCount || 0} // total items loaded
+        next={fetchNextPage}
+        hasMore={!!hasNextPage} // Boolean from your infinite query
+        loader={<Spinner></Spinner>}
+        endMessage={<p className="text-center my-3">No more posts</p>}
       >
-        {isFetchingNextPage ? "Loading..." : "Load More"}
-      </Button>
+        <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={3}>
+          {isLoading && skeletons.map((s) => <GameCardSkeleton key={s} />)}
+          {data?.pages.map((page, index) => (
+            <React.Fragment key={index}>
+              {page.results?.map((game) => (
+                <GameCard key={game.id} game={game}></GameCard>
+              ))}
+            </React.Fragment>
+          ))}
+        </SimpleGrid>
+      </InfiniteScroll>
     </>
   );
 };
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,4 @@
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import type { GameQuery } from "../App";
 import { type FetchResponse } from "../services/api-client";
 import type { Game } from "../services/game-service";
@@ -20,7 +20,6 @@ const useGames = (gameQuery: GameQuery | null) => {
     getNextPageParam: (lastPage, allPages) => {
       return lastPage.results.length > 0 ? allPages.length + 1 : undefined;
     },
-    initialPageParam: 1, //THIS LINE HERE!
     staleTime: 24 * 60 * 60 * 1000, //24h
   });
 };
